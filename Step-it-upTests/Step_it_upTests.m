a43d979167e717671dafc0e89e8c5a6c9e8a7cee
JFT0M <377632523@qq.com>
2015-07-27 22:13:26 +0800

    Initial Commit

---
+++ Step-it-upTests/Step_it_upTests.m
@@ -0,0 +1,39 @@
+//
+//  Step_it_upTests.m
+//  Step-it-upTests
+//
+//  Created by syfll on 7/27/15.
+//  Copyright © 2015 JFT0M. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Step_it_upTests : XCTestCase
+
+@end
+
+@implementation Step_it_upTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
